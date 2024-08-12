36068b6abd321be697fc77a953117155cc220d0e
tsuzukia21 <tsuzukia21@users.noreply.github.com>
2024-08-12 16:06:37 +0900

    change verno

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="st_img_pastebutton",
-    version="0.0.5",
+    version="0.0.6",
     author="tsuzukia",
     author_email="",
     description="Streamlit component that allows you to create button to paste image from clipboard.",
